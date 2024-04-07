db524646c4407ee656a8031647da0201a8a8ead7
KEN <b2p31018.develop@gmail.com>
2024-04-07 23:55:49 +0900

    ステップ4. gem 'devise'を使ったログイン機能の実装（その2）

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  get '/users/dash_boards', to: 'dash_boards#index'
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
